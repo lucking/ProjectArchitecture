2eb08c9145293131a834853b55a4c87025123260
ZM <1531073749@qq.com>
2018-02-01 23:52:44 +0800

    1.5.3
    整理管理类

---
+++ ProjectArchitecture/Model/TestModel/SlimModel.m
@@ -0,0 +1,13 @@
+//
+//  SlimModel.m
+//  ZMArchitecture
+//
+//  Created by ZM on 16/2/25.
+//  Copyright © 2016年 ZM. All rights reserved.
+//
+
+#import "SlimModel.h"
+
+@implementation SlimModel
+
+@end
